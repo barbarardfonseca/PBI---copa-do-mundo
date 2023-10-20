d0ac0913ca7c34c5750044a98977945833eded73
BARBARA ROHR DECOTHE FONSECA <99733121+barbarardfonseca@users.noreply.github.com>
2023-10-20 20:10:04 -0300

    atualização bandeiras

--- ETL/salvar bandeiras paises.py
+++ ETL/salvar bandeiras paises.py
@@ -12,22 +12,23 @@ response = requests.get("https://www.significados.com.br/bandeiras-dos-paises/")
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Encontre todas as imagens no site
-images = soup.find_all("img", {"src": True})
+images = soup.find_all("img", {"src": True, "title": True})
 
 # Baixe cada imagem
 for image in images:
     # Obtenha o URL da imagem
     url = image["src"]
+    name = image["title"]
     url = url.replace("?width=50&blur=10", "")
     # Baixe a imagem
     response = requests.get(url)
 
     # Salve a imagem na pasta de destino
-    filename = os.path.basename(url)
+    filename = os.path.basename(name)
     CompleteUrl = os.path.join(dest_dir, filename)
     CompleteUrl = os.path.normpath(CompleteUrl)
     with open(CompleteUrl, "wb") as f:
-        print(CompleteUrl,'join')
+        # print(CompleteUrl,'join')
         f.write(response.content)
 
 # Imprima uma mensagem de confirmação
